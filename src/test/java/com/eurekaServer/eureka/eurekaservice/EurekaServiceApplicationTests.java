96b5747d8d4310fa33c89e550864d8f36e3f1e95
AndresLopezGarcia <92831067+AndresLopezGarcia@users.noreply.github.com>
2021-11-18 12:57:04 -0500

    eureka server.

---
+++ src/test/java/com/eurekaServer/eureka/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaServer.eureka.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
